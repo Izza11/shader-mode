92a28592ab08c5cd1a04de0547dfadd3495ed7b6
Izza <tariq.izza11@gmail.com>
2019-05-13 06:34:44 -0400

    1.Renamed repository 2. Added processing core folder 3. Added "New Shader Tab" popup item

---
+++ shader-mode/src/processing/mode/shader/ShaderMode.java
@@ -0,0 +1,37 @@
+package processing.mode.shader;
+
+import java.io.File;
+
+import processing.app.Base;
+import processing.mode.java.JavaMode;
+
+public class ShaderMode extends JavaMode {
+
+  public ShaderMode(Base base, File folder) {
+    super(base, folder);
+  }
+  
+  @Override
+  public String getTitle() {
+    return "Shader";
+  }
+  
+  @Override
+  public String[] getExtensions() {
+    return new String[] { "pde", "java", "glsl" };
+  }
+  
+  /*
+  @Override
+  public String getDefaultExtension() {
+	  return "pde";
+  }
+  
+  
+  @Override
+  public String getModuleExtension() {
+	  return "glsl";
+  }
+  */
+  
+}
