7fdbdafef09b1e16954d48b7244383c6b8857414
Izza <tariq.izza11@gmail.com>
2019-09-27 01:50:14 -0400

    Added functionality for new shader tab

--- src/processing/mode/shader/ShaderModeHeader.java
+++ src/processing/mode/shader/ShaderModeHeader.java
@@ -41,6 +41,144 @@ public class ShaderModeHeader extends EditorHeader{
 		// TODO Auto-generated constructor stub
 	}
 	
+	////////////////////////////////////////////////
+	
+	@Override
+	public void rebuildMenu() {
+	    //System.out.println("rebuilding");
+	    if (menu != null) {
+	      menu.removeAll();
+
+	    } else {
+	      menu = new JMenu();
+	      popup = menu.getPopupMenu();
+	      add(popup);
+	      popup.setLightWeightPopupEnabled(true);
+	    }
+	    
+	    JMenuItem item;
+	    final JRootPane rootPane = editor.getRootPane();
+	    InputMap inputMap =
+	      rootPane.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+	    ActionMap actionMap = rootPane.getActionMap();
+
+	    Action action;
+	    String mapKey;
+	    KeyStroke keyStroke;
+	    
+	    /////////////////////////
+		item = Toolkit.newJMenuItemShift(Language.text("editor.header.new_shader_tab"), KeyEvent.VK_N);
+		action = new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				((ShaderSketch) editor.getSketch()).handleNewShaderCode();
+			}
+		};
+		mapKey = "editor.header.new_shader_tab";
+		keyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_N, Toolkit.SHORTCUT_SHIFT_KEY_MASK);
+		inputMap.put(keyStroke, mapKey);
+		actionMap.put(mapKey, action);
+		item.addActionListener(action);
+		menu.add(item);
+		/////////////////////////
+
+	    item = Toolkit.newJMenuItemShift(Language.text("editor.header.new_tab"), KeyEvent.VK_N);
+	    action = new AbstractAction() {
+	      @Override
+	      public void actionPerformed(ActionEvent e) {
+	        editor.getSketch().handleNewCode();
+	      }
+	    };
+	    mapKey = "editor.header.new_tab";
+	    keyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_N, Toolkit.SHORTCUT_SHIFT_KEY_MASK);
+	    inputMap.put(keyStroke, mapKey);
+	    actionMap.put(mapKey, action);
+	    item.addActionListener(action);
+	    menu.add(item);
+
+	    item = new JMenuItem(Language.text("editor.header.rename"));
+	    action = new AbstractAction() {
+	      @Override
+	      public void actionPerformed(ActionEvent e) {
+	        editor.getSketch().handleRenameCode();
+	      }
+	    };
+	    item.addActionListener(action);
+	    menu.add(item);
+
+	    item = Toolkit.newJMenuItemShift(Language.text("editor.header.delete"), KeyEvent.VK_D);
+	    action = new AbstractAction() {
+	      @Override
+	      public void actionPerformed(ActionEvent e) {
+	        Sketch sketch = editor.getSketch();
+	        if (!Platform.isMacOS() &&  // ok on OS X
+	            editor.getBase().getEditors().size() == 1 &&  // mmm! accessor
+	            sketch.getCurrentCodeIndex() == 0) {
+	            Messages.showWarning(Language.text("editor.header.delete.warning.title"),
+	                                 Language.text("editor.header.delete.warning.text"));
+	        } else {
+	          editor.getSketch().handleDeleteCode();
+	        }
+	      }
+	    };
+	    mapKey = "editor.header.delete";
+	    keyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_D, Toolkit.SHORTCUT_SHIFT_KEY_MASK);
+	    inputMap.put(keyStroke, mapKey);
+	    actionMap.put(mapKey, action);
+	    item.addActionListener(action);
+	    menu.add(item);
+
+	    menu.addSeparator();
+
+	    //  KeyEvent.VK_LEFT and VK_RIGHT will make Windows beep
+
+	    mapKey = "editor.header.previous_tab";
+	    item = Toolkit.newJMenuItemExt(mapKey);
+	    action = new AbstractAction() {
+	      @Override
+	      public void actionPerformed(ActionEvent e) {
+	        editor.getSketch().handlePrevCode();
+	      }
+	    };
+	    keyStroke = item.getAccelerator();
+	    inputMap.put(keyStroke, mapKey);
+	    actionMap.put(mapKey, action);
+	    item.addActionListener(action);
+	    menu.add(item);
+
+	    mapKey = "editor.header.next_tab";
+	    item = Toolkit.newJMenuItemExt(mapKey);
+	    action = new AbstractAction() {
+	      @Override
+	      public void actionPerformed(ActionEvent e) {
+	        editor.getSketch().handleNextCode();
+	      }
+	    };
+	    keyStroke = item.getAccelerator();
+	    inputMap.put(keyStroke, mapKey);
+	    actionMap.put(mapKey, action);
+	    item.addActionListener(action);
+	    menu.add(item);
+
+	    Sketch sketch = editor.getSketch();
+	    if (sketch != null) {
+	      menu.addSeparator();
+
+	      ActionListener jumpListener = new ActionListener() {
+	        public void actionPerformed(ActionEvent e) {
+	          editor.getSketch().setCurrentCode(e.getActionCommand());
+	        }
+	      };
+	      for (SketchCode code : sketch.getCode()) {
+	        item = new JMenuItem(code.getPrettyName());
+	        item.addActionListener(jumpListener);
+	        menu.add(item);
+	      }
+	    }
+
+	    Toolkit.setMenuMnemonics(menu);
+	  }
+	
 
 	
 
--- src/processing/mode/shader/ShaderSketch.java
+++ src/processing/mode/shader/ShaderSketch.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 import com.sun.tools.javac.util.ArrayUtils;
 
+import processing.app.Language;
+import processing.app.Messages;
 import processing.app.Sketch;
 import processing.app.SketchCode;
 import processing.app.ui.Editor;
@@ -15,10 +17,12 @@ import processing.app.ui.Editor;
 public class ShaderSketch extends Sketch {
 
 	int codeFilesCount;
+	boolean renamingShaderCode;
 	
 	public ShaderSketch(String path, Editor editor) throws IOException {		
 		super(path, editor);
 		codeFilesCount = 0;
+		renamingShaderCode = false;
 		// TODO Auto-generated constructor stub
 	}
 	
@@ -104,15 +108,222 @@ public class ShaderSketch extends Sketch {
 		    }
 		
 	}
-		  public void getSketchCodeFiles(List<String> outFilenames,
-		                                 List<String> outExtensions) {
+	public void getSketchCodeFiles(List<String> outFilenames, List<String> outExtensions) {
 		    // get list of files in the sketch folder
 		    String list[] = getFolder().list();
 		    String list2[] = getDataFolder().list();
 		    setSketchCodeFiles(outFilenames, outExtensions, list, "code");
 		    if (list2 != null) 
 		    	setSketchCodeFiles(outFilenames, outExtensions, list2, "data");
-		    
-		  }
+	}
+	
+	
+	public void handleNewShaderCode() {
+	    // make sure the user didn't hide the sketch folder
+	    ensureExistence();
+
+	    // if read-only, give an error
+	    if (isReadOnly()) {
+	      // if the files are read-only, need to first do a "save as".
+	      Messages.showMessage(Language.text("new.messages.is_read_only"),
+	                           Language.text("new.messages.is_read_only.description"));
+	      return;
+	    }
+	    renamingShaderCode = true;
+	    renamingCode = false;
+	    // editor.status.edit("Name for new file:", "");
+	    promptForTabName(Language.text("editor.tab.rename.description")+":", "");
+	  }
+		  
+	
+	protected void nameCode(String newName) {
+	    newName = newName.trim();
+	    if (newName.length() == 0) {
+	      return;
+	    }
+
+	    // make sure the user didn't hide the sketch folder
+	    ensureExistence();
+
+	    // Add the extension here, this simplifies some of the logic below.
+	    if (newName.indexOf('.') == -1) {
+	    	if (renamingShaderCode) {
+	    		newName += "." + "glsl";
+	    	} else {
+	    		newName += "." + (renamingCode ? getMode().getDefaultExtension() : getMode().getModuleExtension());
+	    	}	      
+	    }
+	    // if renaming to the same thing as before, just ignore.
+	    // also ignoring case here, because i don't want to write
+	    // a bunch of special stuff for each platform
+	    // (osx is case insensitive but preserving, windows insensitive,
+	    // *nix is sensitive and preserving.. argh)
+	    if (renamingCode) {
+	      if (newName.equalsIgnoreCase(current.getFileName())) {
+	        // exit quietly for the 'rename' case.
+	        // if it's a 'new' then an error will occur down below
+	        return;
+	      }
+	    }
+
+	    if (newName.startsWith(".")) {
+	      Messages.showWarning(Language.text("name.messages.problem_renaming"),
+	                           Language.text("name.messages.starts_with_dot.description"));
+	      return;
+	    }
+
+	    int dot = newName.lastIndexOf('.');    
+	    String newExtension = newName.substring(dot+1).toLowerCase();
+	    if (!getMode().validExtension(newExtension)) {
+	      Messages.showWarning(Language.text("name.messages.problem_renaming"),
+	                           Language.interpolate("name.messages.invalid_extension.description",
+	                           newExtension));
+	      return;
+	    }
+
+	    // Don't let the user create the main tab as a .java file instead of .pde
+	    if (!getMode().isDefaultExtension(newExtension)) {
+	      if (renamingCode) {  // If creating a new tab, don't show this error
+	        if (current == code[0]) {  // If this is the main tab, disallow
+	          Messages.showWarning(Language.text("name.messages.problem_renaming"),
+	                               Language.interpolate("name.messages.main_java_extension.description",
+	                               newExtension));
+	          return;
+	        }
+	      }
+	    }
+
+	    // dots are allowed for the .pde and .java, but not in the name
+	    // make sure the user didn't name things poo.time.pde
+	    // or something like that (nothing against poo time)
+	    String shortName = newName.substring(0, dot);
+	    String sanitaryName = Sketch.sanitizeName(shortName);
+	    if (!shortName.equals(sanitaryName)) {
+	      newName = sanitaryName + "." + newExtension;
+	    }
+
+	    // If changing the extension of a file from .pde to .java, then it's ok.
+	    // http://code.google.com/p/processing/issues/detail?id=776
+	    // A regression introduced by Florian's bug report (below) years earlier.
+	    if (!(renamingCode && sanitaryName.equals(current.getPrettyName()))) {
+	      // Make sure no .pde *and* no .java files with the same name already exist
+	      // (other than the one we are currently attempting to rename)
+	      // http://processing.org/bugs/bugzilla/543.html
+	      for (SketchCode c : code) {
+	        if (c != current && sanitaryName.equalsIgnoreCase(c.getPrettyName())) {
+	          Messages.showMessage(Language.text("name.messages.new_sketch_exists"),
+	                               Language.interpolate("name.messages.new_sketch_exists.description",
+	                               c.getFileName(), getFolder().getAbsolutePath()));
+	          return;
+	        }
+	      }
+	    }
+
+	    File newFile = new File(getFolder(), newName);
+
+	    if (renamingCode) {
+	      if (currentIndex == 0) {
+	        // get the new folder name/location
+	        String folderName = newName.substring(0, newName.indexOf('.'));
+	        File newFolder = new File(getFolder().getParentFile(), folderName);
+	        if (newFolder.exists()) {
+	          Messages.showWarning(Language.text("name.messages.new_folder_exists"),
+	                               Language.interpolate("name.messages.new_folder_exists.description",
+	                               newName));
+	          return;
+	        }
+
+	        // renaming the containing sketch folder
+	        boolean success = getFolder().renameTo(newFolder);
+	        if (!success) {
+	          Messages.showWarning(Language.text("name.messages.error"),
+	                               Language.text("name.messages.no_rename_folder.description"));
+	          return;
+	        }
+	        // let this guy know where he's living (at least for a split second)
+	        current.setFolder(newFolder);
+	        // folder will be set to newFolder by updateInternal()
+
+	        // unfortunately this can't be a "save as" because that
+	        // only copies the sketch files and the data folder
+	        // however this *will* first save the sketch, then rename
+
+	        // moved this further up in the process (before prompting for the name)
+//	        if (isModified()) {
+//	          Base.showMessage("Save", "Please save the sketch before renaming.");
+//	          return;
+//	        }
+
+	        // This isn't changing folders, just changes the name
+	        newFile = new File(newFolder, newName);
+	        if (!current.renameTo(newFile, newExtension)) {
+	          Messages.showWarning(Language.text("name.messages.error"),
+	                               Language.interpolate("name.messages.no_rename_file.description",
+	                               current.getFileName(), newFile.getName()));
+	          return;
+	        }
+
+	        // Tell each code file the good news about their new home.
+	        // current.renameTo() above already took care of the main tab.
+	        for (int i = 1; i < codeCount; i++) {
+	          code[i].setFolder(newFolder);
+	        }
+	       // Update internal state to reflect the new location
+	        updateInternal(sanitaryName, newFolder);
+
+//	        File newMainFile = new File(newFolder, newName + ".pde");
+//	        String newMainFilePath = newMainFile.getAbsolutePath();
+	//
+//	        // having saved everything and renamed the folder and the main .pde,
+//	        // use the editor to re-open the sketch to re-init state
+//	        // (unfortunately this will kill positions for carets etc)
+//	        editor.handleOpenUnchecked(newMainFilePath,
+//	                                   currentIndex,
+//	                                   editor.getSelectionStart(),
+//	                                   editor.getSelectionStop(),
+//	                                   editor.getScrollPosition());
+	//
+//	        // get the changes into the sketchbook menu
+//	        // (re-enabled in 0115 to fix bug #332)
+//	        editor.base.rebuildSketchbookMenusAsync();
+
+	      } else {  // else if something besides code[0]
+	        if (!current.renameTo(newFile, newExtension)) {
+	          Messages.showWarning(Language.text("name.messages.error"),
+	                               Language.interpolate("name.messages.no_rename_file.description",
+	                               current.getFileName(), newFile.getName()));
+	          return;
+	        }
+	      }
+
+	    } else {  // not renaming, creating a new file
+	      try {
+	        if (!newFile.createNewFile()) {
+	          // Already checking for IOException, so make our own.
+	          throw new IOException("createNewFile() returned false");
+	        }
+	      } catch (IOException e) {
+	        Messages.showWarning(Language.text("name.messages.error"),
+	                             Language.interpolate("name.messages.no_create_file.description",
+	                             newFile, getFolder().getAbsolutePath()), e);
+	        return;
+	      }
+	      SketchCode newCode = new SketchCode(newFile, newExtension);
+	      //System.out.println("new code is named " + newCode.getPrettyName() + " " + newCode.getFile());
+	      insertCode(newCode);
+	    }
+
+	    // sort the entries
+	    sortCode();
+
+	    // set the new guy as current
+	    setCurrentCode(newName);
+
+	    // update the tabs
+	    editor.rebuildHeader();
+	    renamingShaderCode = false;
+	  }
+	
+		  
 
 }
