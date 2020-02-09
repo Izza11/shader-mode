a75095ff98cf67f220f072dd3ba3cf8d4246055d
Izza <tariq.izza11@gmail.com>
2020-02-09 02:54:20 -0500

    1. Pop-up asking user's permission to move shader files 2. Backward slash in paths converted to forward for Mac 3. Accepting template naming with and without extension 4. Added post survey link in Shader menu 5. Other minor changes and code cleaning

--- src/processing/mode/shader/ShaderSketch.java
+++ src/processing/mode/shader/ShaderSketch.java
@@ -11,15 +11,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import processing.app.Language;
 import processing.app.Messages;
+import processing.app.Platform;
 import processing.app.Sketch;
 import processing.app.SketchCode;
 import processing.app.ui.Editor;
+import java.awt.event.ActionEvent;
 
 public class ShaderSketch extends Sketch {
 
@@ -34,36 +37,19 @@ public class ShaderSketch extends Sketch {
     renamingShaderCode = false;
   }
 
-  
-  @Override
   protected void load() {
     moveShaderFilesToRoot();
-    
     super.load();
-    
-    /*
-    // Adding shader files that could be inside data
-    folders  = new ArrayList<String>();
-    List<String> filenames = new ArrayList<>();
-    List<String> extensions = new ArrayList<>();    
-    setSketchCodeFiles(getDataFolder(), filenames, extensions);
-
-    for (int i = 0; i < filenames.size(); i++) {
-      String filename = filenames.get(i);
-      String extension = extensions.get(i);
-      String path = folders.get(i);
-      SketchCode newCode = new SketchCode(new File(path, filename), extension);
-      insertCode(newCode);
-    }
-    */
   }
   
   private void moveShaderFilesToRoot() {
     File dataFolderTmp = new File(getFolder(), "data");
     // get list of files in the sketch folder
     String list[] = dataFolderTmp.list();
-    if (list == null) return;
-    
+    if (list == null) 
+    	return;    
+
+    boolean allowed = false;
     for (String filename : list) {
       // Ignoring the dot prefix files is especially important to avoid files
       // with the ._ prefix on Mac OS X. (You'll see this with Mac files on
@@ -78,6 +64,13 @@ public class ShaderSketch extends Sketch {
       // now strip off the .pde and .java extensions
       String extension = "glsl";
       if (base.toLowerCase().endsWith("." + extension)) {
+    	  if (!allowed) {
+    		  if (!askUser()) {
+    			  return;
+    		  } else {
+    			  allowed = true;
+    		  }   			  
+    	   }
         base = base.substring(0, base.length() - (extension.length() + 1));
         if (isSanitaryName(base)) {
           File srcFilename = new File(dataFolderTmp, filename);
@@ -85,78 +78,6 @@ public class ShaderSketch extends Sketch {
           srcFilename.renameTo(dstFilename);
         }
       }      
-    }
-  }
-
-  
-  @Override
-  public void getSketchCodeFiles(List<String> outFilenames, List<String> outExtensions) {
-    super.getSketchCodeFiles(outFilenames, outExtensions);
-    
-    /*
-    // get list of files in the sketch folder
-    String list[] = getDataFolder().list();
-    if (list == null) return;
-    
-    for (String filename : list) {
-      // Ignoring the dot prefix files is especially important to avoid files
-      // with the ._ prefix on Mac OS X. (You'll see this with Mac files on
-      // non-HFS drives, i.e. a thumb drive formatted FAT32.)
-      if (filename.startsWith(".")) continue;
-
-      // Don't let some wacko name a directory blah.pde or bling.java.
-      if (new File(getFolder(), filename).isDirectory()) continue;
-
-      // figure out the name without any extension
-      String base = filename;
-      // now strip off the .pde and .java extensions
-      for (String extension : getMode().getExtensions()) {
-        if (base.toLowerCase().endsWith("." + extension)) {
-          base = base.substring(0, base.length() - (extension.length() + 1));
-
-          // Don't allow people to use files with invalid names, since on load,
-          // it would be otherwise possible to sneak in nasty filenames. [0116]
-          if (isSanitaryName(base)) {
-            if (outFilenames != null) outFilenames.add(filename);
-            if (outExtensions != null) outExtensions.add(extension);
-          }
-        }
-      }
-    }
-    */
-  }  
-  
-  
-  private void setSketchCodeFiles(File inFolder, List<String> outFilenames, List<String> outExtensions) {
-    String list[] = inFolder.list();
-    if (list == null) return;
-    for (String filename : list) {
-      // Ignoring the dot prefix files is especially important to avoid files
-      // with the ._ prefix on Mac OS X. (You'll see this with Mac files on
-      // non-HFS drives, i.e. a thumb drive formatted FAT32.)
-      if (filename.startsWith("."))
-        continue;
-
-      // Don't let some wacko name a directory blah.pde or bling.java.
-      if (new File(getFolder(), filename).isDirectory())
-        continue;
-
-      // figure out the name without any extension
-      String base = filename;
-      // now strip off the .pde and .java extensions
-      for (String extension : getMode().getExtensions()) {
-        if (base.toLowerCase().endsWith("." + extension)) {
-          base = base.substring(0, base.length() - (extension.length() + 1));
-
-          // Don't allow people to use files with invalid names, since on load,
-          // it would be otherwise possible to sneak in nasty filenames. [0116]
-          if (isSanitaryName(base)) {
-            if (outFilenames != null) outFilenames.add(filename);
-            if (outExtensions != null) outExtensions.add(extension);
-            folders.add(inFolder.getAbsolutePath());
-          }
-        }
-      }
     }
   }
 
@@ -227,13 +148,14 @@ public class ShaderSketch extends Sketch {
         new Object[] { Language.getPrompt("ok"), Language.getPrompt("cancel") }, field);
 
     if (userReply == JOptionPane.OK_OPTION) {
-      nameCode(field.getText());
-    }
-    return field.getText();
+      return nameShaderCode(field.getText());
+    } 
+    return "";
   }
 
   
   public String handleNewShaderCode() {
+	  System.out.println("entered handlenewsshadercode");
     // make sure the user didn't hide the sketch folder
     ensureExistence();
 
@@ -251,19 +173,78 @@ public class ShaderSketch extends Sketch {
   }
   
 
-  @Override
-  protected void nameCode(String newName) {
+  protected String nameShaderCode(String newName) {
     newName = newName.trim();
     if (newName.length() == 0) {
-      return;
+      return "";
     }
     
     if (renamingShaderCode && newName.indexOf('.') == -1) {
-      newName += "." + "glsl";
-    }
+      newName += "." + "glsl";    
+    } 
     
-    super.nameCode(newName);
-    
+    super.nameCode(newName);    
     renamingShaderCode = false;
+    return newName;
   }
+  
+  protected boolean askUser() {
+	  if (shaderEditor == null)
+		  return false;;
+	  
+	    if (!Platform.isMacOS()) {
+	      String prompt = Language.interpolate("Shader files will be moved to root sketch folder. Do you want to proceed?");
+	      int result = JOptionPane.showConfirmDialog(shaderEditor, prompt, Language.text("menu.file.close"),
+	          JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+
+	      if (result == JOptionPane.YES_OPTION) {
+	       return true;
+	      } else if (result == JOptionPane.CANCEL_OPTION || result == JOptionPane.CLOSED_OPTION) {
+	        return false;
+	      } else {
+	        return false;
+	      }
+
+	    } else {
+	      // This code is disabled unless Java 1.5 is being used on Mac OS X
+	      // because of a Java bug that prevents the initial value of the
+	      // dialog from being set properly (at least on my MacBook Pro).
+	      // The bug causes the "Don't Save" option to be the highlighted,
+	      // blinking, default. This sucks. But I'll tell you what doesn't
+	      // suck--workarounds for the Mac and Apple's snobby attitude about it!
+	      // I think it's nifty that they treat their developers like dirt.
+
+	      // Pane formatting adapted from the quaqua guide
+	      // http://www.randelshofer.ch/quaqua/guide/joptionpane.html
+	      JOptionPane pane = new JOptionPane("<html> " + "<head> <style type=\"text/css\">"
+	          + "b { font: 13pt \"Lucida Grande\" }" + "p { font: 11pt \"Lucida Grande\"; margin-top: 8px }"
+	          + "</style> </head>" + "<b>" + Language.interpolate("Shader files will be moved to root sketch folder. Do you want to proceed?", getName())
+	          + "</b>" + "<p>" + Language.text("save.hint") + "</p>", JOptionPane.QUESTION_MESSAGE);
+
+	      String[] options = new String[] { Language.text("Yes"), Language.text("Cancel"), // put No here
+	          Language.text("No") };
+	      pane.setOptions(options);
+
+	      // highlight the safest option ala apple hig
+	      pane.setInitialValue(options[0]);
+
+	      // on macosx, setting the destructive property places this option
+	      // away from the others at the lefthand side
+	      pane.putClientProperty("Quaqua.OptionPane.destructiveOption", Integer.valueOf(2));
+
+	      JDialog dialog = pane.createDialog(shaderEditor, null);
+	      dialog.setVisible(true);
+
+	      Object result = pane.getValue();
+	      if (result == options[0]) { // Yes already filled survey
+	        return true;
+	      } else if (result == options[2]) { // Haven't filled survey
+	        return false;
+
+	      } else { // cancel?
+	        return false;
+	      }
+	    }
+  }
+  
 }
